85c0a5fd04b62037eb480ec89f47ad679088f7b0
BenRa <benjamin.raethlein@gmail.com>
2015-04-18 15:33:35 +0200

    started with statistics card

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,7 @@
 angular.module('crowdpaths', [])
     .controller('MainController', function ($scope,  $window, $log) {
         $scope.toolbarTitle = "Android Tutorial by Clark Gable";
+        $scope.durationTime = "3h 05m";
+        $scope.usersCompleted = 1337;
     });
 

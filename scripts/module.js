737716b4acd8e6ee0a57f0a86b07e1ef8afcb3b4
BenRa <benjamin.raethlein@gmail.com>
2015-04-18 14:45:10 +0200

    basic card wrapper

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,5 @@
 angular.module('crowdpaths', [])
     .controller('MainController', function ($scope,  $window, $log) {
-
+        $scope.toolbarTitle = "Android Tutorial by Clark Gable";
     });
 
